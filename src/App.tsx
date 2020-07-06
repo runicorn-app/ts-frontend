11fb6c1f703c3f8bd1d4b12b39cc68cb18df6537
Frank Li <lifrank@grinnell.edu>
2020-07-06 11:03:51 -0500

    added stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,64 @@
-import React, { SetStateAction, useState} from 'react';
+import React, { FunctionComponent, SetStateAction, useState } from 'react';
 import { Route, Switch as RouterSwitch, Link } from 'react-router-dom';
-import { CssBaseline, Toolbar, Button, Switch } from "@material-ui/core";
+import { CssBaseline, List, ListItem, ListItemText, Theme } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import NavBar from './components/NavBar';
-import Home from './components/Home';
+import Home from './pages/Home';
 import Login from './components/Login';
 
-const DarkTheme = () => createMuiTheme({
-    palette: {
-      type: "dark",
-    },
-});
-
-const LightTheme = () => createMuiTheme({
-  palette: {
-    type: "light",
-  },
-});
 
 function App() {
   let [isDark, setIsDark]: [boolean, React.Dispatch<SetStateAction<boolean>>] = useState<boolean>(false);
 
-  function handleDarkSwitch(e: React.ChangeEvent<HTMLInputElement>) {
-    setIsDark(e.target.checked);
-  }
+  type NavEntry = readonly [string, string];
+
+  const items: NavEntry[] = [
+    ['Home', '/'],
+    ['Login', '/login'],
+  ]
+
+  const routes: Record<string, FunctionComponent > = {
+    'Home': Home,
+    'Login': Login
+  };
 
   return (
     <ThemeProvider theme={isDark? DarkTheme(): LightTheme()}>
       <CssBaseline />
-      <NavBar>
-        <Toolbar>
-          <Button component={Link} to="/">
-            Home
-          </Button>
-          <Button component={Link} to="/login">
-            Login
-          </Button>
-          <Switch
-            checked={isDark}
-            onChange={handleDarkSwitch}
-          />
-        </Toolbar>
+      <NavBar setIsDark={setIsDark} isDark={isDark} >
+        {(c: string) => (
+          <List className={c}>
+            {items.map((entry: NavEntry, i: number) => (
+              <ListItem button component={Link} to={entry[1]}>
+                <ListItemText primary={entry[0]}/>
+              </ListItem>
+            ))}
+          </List>
+        )}
       </NavBar>
       <RouterSwitch>
-        <Route exact path={'/login'} component={Login}/>
-        <Route exact path={'/'} component={Home} />
+        {items.map((entry: NavEntry, i: number) => (
+          <Route exact path={entry[1]} component={routes[entry[0]]} />
+        ))}
       </RouterSwitch>
     </ThemeProvider>
   );
+}
+
+function DarkTheme(): Theme {
+  return createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
+}
+
+function LightTheme(): Theme {
+  return createMuiTheme({
+    palette: {
+      type: "light",
+    },
+  });
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,74 @@
-import React, { Fragment } from 'react';
-import { AppBar, Toolbar } from "@material-ui/core";
+import React, { Fragment, useState, SetStateAction } from 'react';
+import { AppBar, Drawer, Switch, Toolbar, Divider, IconButton, Typography } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import MenuIcon from '@material-ui/icons/Menu';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import Brightness2Icon from '@material-ui/icons/Brightness2';
 
 
 export interface NavBarProps {
-  children: React.ReactNode;
+  children: (c: string) => React.ReactNode;
+  setIsDark: React.Dispatch<SetStateAction<boolean>>;
+  isDark: boolean;
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  DrawerToolBar: {
+    userSelect: "none",
+  },
+  MenuButton: {},
+  List: {
+    width: 250,
+  },
+  Title: {
+    flex: 1,
+  },
+  fillSpace: {
+    flex: 1,
+  }
+}));
+
 function NavBar(props: NavBarProps) {
+  let [open, setOpen]: [boolean, React.Dispatch<SetStateAction<boolean>>] = useState<boolean>(false);
+
+  const { isDark, setIsDark } = props
+  const classes = useStyles();
+
+  function handleDarkSwitch(e: React.ChangeEvent<HTMLInputElement>) {
+    setIsDark(e.target.checked);
+  }
+
   return (
     <Fragment>
       <AppBar>
-        {props.children}
+        <Toolbar>
+          <IconButton onClick={() => setOpen(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant={'h6'} className={classes.Title}>
+            Runicorn
+          </Typography>
+          <Switch
+            checked={isDark}
+            onChange={handleDarkSwitch}
+            icon={<WbSunnyIcon />}
+            checkedIcon={<Brightness2Icon />}
+          />
+        </Toolbar>
       </AppBar>
+      <Drawer anchor={'left'} open={open}>
+        <Toolbar className={classes.DrawerToolBar}>
+          <Typography variant={'h5'} className={classes.fillSpace}>
+            Runicorn
+          </Typography>
+          <IconButton edge={'end'} className={classes.MenuButton} onClick={() => setOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        {props.children(classes.List)}
+      </Drawer>
       <Toolbar />
     </Fragment>
   )
