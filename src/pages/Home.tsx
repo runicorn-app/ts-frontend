31bd4d28d72e9d9df9157dbbf3107979a1c74687
Robert Lorch <lorchrob@gmail.com>
2020-07-08 23:35:03 -0700

    Add basic home/login/register stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, FormControl, Button, Typography } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
-import { Container, Box } from '@material-ui/core';
+const useStyles = makeStyles((theme: Theme) => ({
+  homePage: {
+    marginTop: theme.spacing(4),
+  }
+}));
 
-function Home() {
-  return (
+export const Home = () => {
+  const classes = useStyles();
+
+  return ( 
     <Container>
-      <Box>
-        <p>
-          Hello
-        </p>
-      </Box>
+    <Grid container justify={'center'} className={classes.homePage}>
+     <div>
+        <Typography variant={'h3'} align={'center'}>Runicorn</Typography>
+        <Typography variant={'body1'} align={'center'}>
+          Create a runicorn profile, log runs and view other runners' profiles
+        </Typography>
+        <Grid container justify={'center'}>
+          <Button href="/register"> 
+            Register
+          </Button>
+          <Button href="/login">
+            Login
+          </Button>
+        </Grid>
+      </div>
+      </Grid>
     </Container>
-  )
-}
+  );
+};
 
 export default Home;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
-import { Container, Box } from '@material-ui/core';
+import { Field, Form, Formik } from 'formik';
+import { Container, Grid, FormControl, Button, Typography } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  loginPage: {
+    marginTop: theme.spacing(4),
+  }
+}));
 
 function Login() {
+  const classes = useStyles();
+
   return (
     <Container>
-      <Box>
-        <p>
-          Login Page
-        </p>
-      </Box>
+      <Grid container justify={'center'} className={classes.loginPage}>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={values => {
+            const errors: Record<string, string> = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            } else if (!values.password) {
+              errors.email = 'Required';
+            } else if (values.password.length < 8) {
+              errors.email = 'Must be at least 8 Characters';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <FormControl component={Form}>
+              <Typography variant={'h4'}>
+                Login
+              </Typography>
+              <Field type="email" name="email" label="email" component={TextField} />
+              <Field type="password" name="password" label="password" component={TextField} />
+              <Button type="submit" disabled={isSubmitting}>
+                Submit
+              </Button>
+            </FormControl>
+          )}
+        </Formik>
+      </Grid>
     </Container>
   )
 }
